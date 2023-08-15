469653f9a05e70b6a35fc3cdeccd5084e1c3758f
Gabriel Vieira <72738617+gabrielgits@users.noreply.github.com>
2023-08-14 21:41:09 -0500

    export tables

--- lib/bform.dart
+++ lib/bform.dart
@@ -1,10 +1,15 @@
 library bform;
 
 export 'src/form/bform_obj.dart';
+
 export 'src/bform_appbar.dart';
 export 'src/bform_footer.dart';
+
 export 'src/form/bform_checkbox.dart';
 export 'src/form/bform_form.dart';
 export 'src/form/bform_group_radio.dart';
 export 'src/form/bform_select.dart';
 export 'src/form/bform_text_input.dart';
+
+export 'src/tables/bform_table.dart';
+export 'src/tables/bform_table_cell.dart';
