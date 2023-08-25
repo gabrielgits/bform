a8fe8fd0214a5be3a2564dd68a23c30d653622bc
Gabriel Vieira <72738617+gabrielgits@users.noreply.github.com>
2023-08-24 19:32:24 -0500

    Update bform_table.dart

--- lib/src/tables/bform_table.dart
+++ lib/src/tables/bform_table.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'bform_table_cell.dart';
 
 class BformTable extends StatelessWidget {
-  final List<String> titles;
-  final List<List<String>> rows;
+  final List<Widget> headers;
+  final List<List<Widget>> rows;
   final List<Widget?> actionButtons;
-  final String? actionTitle;
+  final Color? colorHeader;
   final Color? color;
   const BformTable({
     super.key,
-    required this.titles,
+    required this.headers,
     required this.rows,
     this.actionButtons = const [],
-    this.actionTitle,
+    this.colorHeader,
     this.color,
   });
 
@@ -24,21 +24,21 @@ class BformTable extends StatelessWidget {
       children: <TableRow>[
         TableRow(
           children: <Widget>[
-            for (var title in titles) BformTableCell(child: Text(title)),
-            if (actionTitle != null)
-              BformTableCell(child: Text(actionTitle ?? '')),
+            for (final header in headers)
+              BformTableCell(
+                child: header,
+                color: colorHeader,
+              ),
           ],
         ),
-        for (int i = 0; i < rows.length; i++)
+        for (final row in rows)
           TableRow(
             children: <Widget>[
-              for (var column in rows[i])
+              for (var column in row)
                 BformTableCell(
                   color: color,
-                  child: Text(column),
+                  child: column,
                 ),
-              if (actionButtons[i] != null)
-                BformTableCell(child: actionButtons[i] ?? const SizedBox()),
             ],
           ),
       ],
